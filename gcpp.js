98588b4c3036c6a464b8aae47d8453ac10aada73
chaixb <1481569358@qq.com>
2019-10-18 22:09:52 +0800

    join arguments

--- gcpp.js
+++ gcpp.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process")
-const [_, __, message = "updates"] = process.argv
+
+const argv = process.argv.slice(2)
+const message = argv.length > 0 ? argv.join(" ") : "updates"
 
 execSync("git add .", { stdio: "inherit" })
-execSync("git commit -m " + message, { stdio: "inherit" })
+execSync("git commit -m '" + message + "'", { stdio: "inherit" })
 execSync("git pull --rebase", { stdio: "inherit" })
 execSync("git push", { stdio: "inherit" })
