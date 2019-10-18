36452ac476ba8cacb1f438cb5c31d020725fa5ce
chaixb <1481569358@qq.com>
2019-10-18 22:00:15 +0800

    updates

--- gcpp.js
+++ gcpp.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process")
-console.log(process.argv)
 const [_, __, message = "updates"] = process.argv
 
 execSync("git add .", { stdio: "inherit" })
 execSync("git commit -m " + message, { stdio: "inherit" })
+execSync("git pull --rebase", { stdio: "inherit" })
+execSync("git push", { stdio: "inherit" })
